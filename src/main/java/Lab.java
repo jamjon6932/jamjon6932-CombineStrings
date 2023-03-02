3f5e7ec7034e472656dbbea68991da9d85116e16
James Jones <jamjon6932@students.ecpi.edu>
2023-03-02 17:29:17 +0000

    Lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -8,7 +8,10 @@ public class Lab {
      * @param b the second part of the string to be combined.
      * @return the combination of strings a and b.
      */
+    
+    
     public String combine(String a, String b){
-        return "";
+        String c = a + b;
+        return c;
     }
 }
